e59861c85e64867e4ce14834c8a36526bc2aa6cb
Vadser <vadser1999@gmail.com>
2017-12-29 13:32:58 +0300

    add

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -56,6 +56,6 @@
   user: "user_name", # overrides user setting above
    keys: %w(/home/user_name/.ssh/id_rsa),
   forward_agent: false,
-  auth_methods: %w(publickey password)
+  auth_methods: %w(publickey password),
  password: "vadser"
  }
