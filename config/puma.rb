20189f655d9b517b7f3c7eb60bb10f950e4a3ead
Vadser <vadser1999@gmail.com>
2017-12-30 15:46:52 +0300

    add

--- config/puma.rb
+++ config/puma.rb
@@ -7,8 +7,8 @@
 threads 1, 6
 workers 1
 
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+app_dir = "/var/www/pushkin/"
+shared_dir = "#{app_dir}/shared/tmp/"
 
 
 # Default to production
